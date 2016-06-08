19d1e0eda082b9e9e05d9b36b83556d5d13ddd1f
allensu <workallensu@gmail.com>
2016-06-07 23:13:12 -0400

    Etch a Sketch skeleton done

--- Etch/script.js
+++ Etch/script.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
 	
-	size = 10;
-	for (i = 1; i < size; i++){	
-		$('#grid').append('<tr>');
-		for (j = 1; j < size; j++){
-			$('#grid').append('<td class="box"/>');
+	var screenWidth = $(window).width() * .45
+
+	size = 20;
+		$('#grid').append('<tr></tr>');
+		for (j = 0; j < size; j++){
+			$('tr').append('<td class="box"/>');
 		}
-		$('#grid').append('</tr>');
+
+	for (i = 1; i < size; i++){	
+		$('tr:last').clone().appendTo('#grid');
 	}
-})
+
+	boxSizes = ((screenWidth - (3 * 4) * size) / size);
+
+	$('td.box').css({
+		"width": boxSizes,
+		"height": boxSizes
+	})
+	$('td.box').mouseover(function(){
+		$(this).css('background-color','blue');
+	});
+});
+
