6dcc203131f23fdfcf1cac16f843b514bb3f034a
allensu1994 <workallensu@gmail.com>
2016-06-08 12:48:56 -0400

    Generate grid button added

--- Etch/script.js
+++ Etch/script.js
@@ -1,25 +1,36 @@
-$(document).ready(function(){
+var size = 20;
+var screenWidth = $(window).width() * .5;
 	
-	var screenWidth = $(window).width() * .45
+function gridGenerate(){
+	$('.grid').remove();
+	$('#wrapper').append('<div class="grid"></div>');
 
-	size = 20;
-		$('#grid').append('<tr></tr>');
-		for (j = 0; j < size; j++){
-			$('tr').append('<td class="box"/>');
-		}
-
-	for (i = 1; i < size; i++){	
-		$('tr:last').clone().appendTo('#grid');
+	$('.grid').append('<tr></tr>');
+	for (j = 0; j < size; j++){
+		$('tr').append('<td class="box"/>');
 	}
 
-	boxSizes = ((screenWidth - (3 * 4) * size) / size);
+	for (i = 1; i < size; i++){	
+		$('tr:last').clone().appendTo('.grid');
+	}
+
+	boxSizes = ((screenWidth - ((3 * 4) * size)) / size);
 
 	$('td.box').css({
 		"width": boxSizes,
 		"height": boxSizes
 	})
 	$('td.box').mouseover(function(){
-		$(this).css('background-color','blue');
+		$(this).css('background-color','#34495e');
 	});
+}
+
+function newGrid(){
+	size = prompt('How large do you want your square?');
+	gridGenerate();
+}
+
+$(document).ready(function(){
+	gridGenerate();
 });
 
